9daf7d114c7d87ae0b04b40fa19e27652f61a290
raethlo <roman.rostar@gmail.com>
2014-06-18 11:38:00 +0200

    adds comments

--- Rostar.Spot.Anagram/Program.cs
+++ Rostar.Spot.Anagram/Program.cs
@@ -24,6 +24,13 @@ namespace Rostar.Spot.Anagram
 
     public static class Extensions
     {
+        /// <summary>
+        /// Extension method, that checks if a given string is an anagram of 
+        /// "this" string
+        /// </summary>
+        /// <param name="str">current string</param>
+        /// <param name="anag">anagram string</param>
+        /// <returns></returns>
         public static bool HasAnagram(this string str,string anag)
         {
             if(str.Length != anag.Length)
@@ -34,6 +41,13 @@ namespace Rostar.Spot.Anagram
             return string.IsNullOrEmpty(res);
         }
 
+        /// <summary>
+        /// Extension method that chooses anagrams of a string from a given list of
+        /// candidates
+        /// </summary>
+        /// <param name="str"></param>
+        /// <param name="words"></param>
+        /// <returns></returns>
         public static List<string> ChooseAnagrams(this string str, List<string> words)
         {
             return words.Where(w => w.HasAnagram(str) == true).ToList();;
